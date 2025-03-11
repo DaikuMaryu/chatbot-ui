5ab6e80f245130481723eee7667b31579b0d5562
DaikuMaryu <jetjaguar1015@gmail.com>
2025-03-11 14:07:38 -0400

    Updated next.config.mjs to allow iFrame embedding

---
+++ next.config.mjs
@@ -0,0 +1,44 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+module.exports = withBundleAnalyzer(
+  withPWA({
+    reactStrictMode: true,
+    images: {
+      remotePatterns: [
+        {
+          protocol: "http",
+          hostname: "localhost",
+        },
+        {
+          protocol: "http",
+          hostname: "127.0.0.1",
+        },
+        {
+          protocol: "https",
+          hostname: "**",
+        },
+      ],
+    },
+    experimental: {
+      serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
+    },
+    // 🔹 Allow iFrame embedding for Carrd
+    async headers() {
+      return [
+        {
+          source: "/(.*)",
+          headers: [
+            { key: "X-Frame-Options", value: "ALLOWALL" },
+            { key: "Content-Security-Policy", value: "frame-ancestors 'self' https://psiprototype.carrd.co;" },
+          ],
+        },
+      ];
+    },
+  })
+);
